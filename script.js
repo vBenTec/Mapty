c016dd2c819f6b3ddd0526e690db9d6a596ddd26
Benjamin Scharf <ben.project.it@gmail.com>
2022-01-17 18:18:33 +0000

    Implementing form functionality on map click

--- script.js
+++ script.js
@@ -20,6 +20,37 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapE) {
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        // console.log(mapE);
+        // const { lat, lng } = mapE.latlng;
+        // console.log(lat, lng);
+
+        // L.marker([lat, lng])
+        //   .addTo(map)
+        //   .bindPopup(
+        //     L.popup({
+        //       maxWidth: 250,
+        //       minWidth: 100,
+        //       autoClose: false,
+        //       closeOnClick: false,
+        //       className: 'running-popup',
+        //     })
+        //   )
+        //   .setPopupContent('Workout')
+        //   .openPopup();
+      });
     },
     function () {
       alert('Could not get the position');
