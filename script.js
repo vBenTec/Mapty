aa23c1485b30aab44fe6e0d5a35b6191fd5e215e
Benjamin Scharf <ben.project.it@gmail.com>
2022-01-17 16:06:27 +0000

    Integrating leaftlet library npm

--- script.js
+++ script.js
@@ -11,9 +11,17 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  function () {},
-  function () {
-    alert('Could not get the position');
-  }
-);
+// Testing if the gelocation does exist
+if (navigator.geolocation)
+  // Get geolocation
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get the position');
+    }
+  );
